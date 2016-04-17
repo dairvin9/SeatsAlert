199752424fa13d99670978ee1ef570d9f25d65fc
dairvin9 <dairvin9@tamu.edu>
2016-04-17 06:35:19 -0500

    Made manifest, began content, empty background.js

---
+++ content.js
@@ -0,0 +1,8 @@
+// content.js
+
+// Basic Annoying Hello World Extension
+//alert("Hello from your Chrome extension!")
+
+// Uncomment this to ask fo rhe user's email. 
+//var userEmail = window.prompt("What's your email address? Don't worry, my developer was too tired to write a back end to steak and sell your personal information. ","dairvin9@tamu.edu");
+
